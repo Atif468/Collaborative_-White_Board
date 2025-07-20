bd9bd3545c80796dece0cb051d60a1550ffe5f8b
Atif468 <withatifansari@gmail.com>
2025-07-20 16:05:21 +0530

    All set 2

--- server/utils/connection.js
+++ server/utils/connection.js
@@ -4,10 +4,11 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    // Connect to MongoDB using the connection URI from environment variables
-    const connect = await mongoose.connect(process.env.MONGODB_CONNECTION_URI );
-    console.log(`MongoDB connected`);
-  } catch (error) {
+    // Connect to MongoDB using the connection URI from environment variables 
+    const connect = await mongoose.connect(process.env.MONGODB_CONNECTION_URI, () => {
+      console.log("MongoDB connection established successfully");
+    });
+   } catch (error) {
     console.error("MongoDB connection error:", error.message);
   }
 };
