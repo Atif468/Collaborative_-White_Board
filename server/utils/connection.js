0f538bf507e1868f64f8dd06be2fbfed5c2d9298
Atif468 <withatifansari@gmail.com>
2025-07-20 15:05:06 +0530

    correct build command

--- server/utils/connection.js
+++ server/utils/connection.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
+    // Connect to MongoDB using the connection URI from environment variables
     const connect = await mongoose.connect(process.env.MONGODB_CONNECTION_URI );
     console.log(`MongoDB connected`);
   } catch (error) {
